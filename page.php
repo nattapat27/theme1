3dec966279e35da7bcb6eb034c4389ea71022f72
nattapat27 <nattapat27@gmail.com>
2018-08-21 22:26:44 +0700

    [ADD]content from post

--- page.php
+++ page.php
@@ -1,16 +1,25 @@
-<?php 
+<?php
     get_header();
-    if(have_posts()) :
-        while(have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php if(is_front_page()){ 
-            include( get_home_template() );
-            get_home_template();
-        } 
-        the_content();?>
-    <?php    endwhile;
-    else :
-        echo '<p>No content found</p>';
-    endif;    
+    if(is_front_page()){
+        slider_template();
+        $args = array(
+            'post_status' => array('publish')
+        );
+        $query = new WP_Query($args);
+        $i=0;
+        if($query->have_posts()){
+            while($query->have_posts()){
+                $query->the_post();
+                echo get_the_title($i).'<br>';
+                echo get_post_field('post_content', $i).'<br>';
+                $i++;
+            }
+        }
+        else{
+            echo '<p>No Contents</p>';
+        }
+    }
+    else{
+
+    }
     get_footer();
-?>
